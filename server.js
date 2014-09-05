30436eea1fa601290f04a2a6421e96d54f8a5ec3
Kevin Hwang <k3vinhwang@gmail.com>
2014-09-04 18:43:02 -0700

    Add basic libraries and get a basic app loaded

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var app = express();
+
+// Configuration
+
+app.use("/static", express.static(__dirname + "/static"));
+
+app.engine("jade", require("jade").__express);
+app.set("view engine", "jade");
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+var server = app.listen(3000, function () {
+  console.log("Listening on port %d", server.address().port);
+});
