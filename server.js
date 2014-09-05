408be3ce76e29641cf47c9c9897caa5e57efa9f6
Kevin Hwang <k3vinhwang@gmail.com>
2014-09-04 20:26:37 -0700

    Add some skeleton methods

--- server.js
+++ server.js
@@ -24,8 +24,19 @@ app.use(express.static(__dirname+ "/static"));
 app.engine("jade", require("jade").__express);
 app.set("view engine", "jade");
 
+// Routing
+
 app.get("/", function (req, res) {
   res.render("index");
+});
+
+app.get("/room/:id", function (req, res) {
+  // Enter a room with the given name
+  var roomId = req.params.name;
+});
+
+app.post("/create", function (req, res) {
+  // Create a new room
 });
 
 var server = app.listen(3000, function () {
