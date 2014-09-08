68ef1ba01afa16d3fc8e47f49a76a1853a086664
Kevin Hwang <k3vinhwang@gmail.com>
2014-09-08 00:43:37 -0700

    Clicking the video makes a screenshot

--- static/js/room.js
+++ static/js/room.js
@@ -13,8 +13,30 @@ navigator.getUserMedia = (navigator.getUserMedia ||
 
 // Webcam capture
 
+var video = document.querySelector("video");
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+var img = document.querySelector("#screenshot");
+var videoSource = null;
+var localMediaStream = null;
+
+var snapshot = function () {
+  if (localMediaStream) {
+    ctx.drawImage(video, 0, 0);
+    img.src = canvas.toDataURL("image/png");
+  }
+};
+
+video.onloadedmetadata = function (e) {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  img.width = video.videoWidth;
+  img.height = video.videoheight;
+};
+
+video.addEventListener("click", snapshot, false);
+
 MediaStreamTrack.getSources(function (sourceInfos) {
-  var videoSource = null;
 
   for (var i = 0; i < sourceInfos.length; i++) {
     var sourceInfo = sourceInfos[i];
@@ -30,10 +52,9 @@ MediaStreamTrack.getSources(function (sourceInfos) {
     }
   };
 
-  window.navigator.getUserMedia(constraints, function (localMediaStream) {
-    console.log("yes");
-    var video = document.querySelector("video");
-    video.src = window.URL.createObjectURL(localMediaStream);
+  window.navigator.getUserMedia(constraints, function (stream) {
+    video.src = window.URL.createObjectURL(stream);
+    localMediaStream = stream;
   }, function (e) {
     console.log("Error :(", e );
   });
