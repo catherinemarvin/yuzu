4675ac4060036edf347e3f0934849b8cda0bbc02
Kevin Hwang <k3vinhwang@gmail.com>
2014-09-07 19:02:56 -0700

    Add some socketIO

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require("express");
-
 var app = express();
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
 
 // Database setup
 
@@ -36,6 +39,15 @@ app.get("/join", function (req, res) {
   res.render("room");
 });
 
-var server = app.listen(3000, function () {
+server.listen(3000, function () {
   console.log("Listening on port %d", server.address().port);
+});
+
+// SocketIO
+
+io.on("connection", function (socket) {
+  socket.emit("news", { hello: "world" });
+  socket.on("my other event", function (data) {
+    console.log(data);
+  });
 });
